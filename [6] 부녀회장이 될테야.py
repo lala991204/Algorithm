e3b4121941caa635ccab41e8dbdff77820ec54d2
lala991204 <oyunjeong7@gmail.com>
2024-06-05 00:11:23 +0900

    Update2 [백준] 2775. 부녀회장이 될테야

--- [6] 부녀회장이 될테야.py
+++ [6] 부녀회장이 될테야.py
@@ -1,17 +1,23 @@
 '''
-IF you live in a-th floor, room b, you have to live with people of sum for from room 1 to room b.
+<Description>
+IF you live in k-th floor, room n, you have to live with people of sum for from room 1 to room n.
 Now, there are 0-th floor or higher, room 1 or higher.
 And i-people live in 0-th floor, room i.
+For given the test case T, get the number of people lived in k-th floor, room n.
 
-example)
-1층의 3호 -> 0층: [1,1,1]
-2층의 3호 -> 1층: [1,1,1] -> 0층: [3,2,1] 
-3층의 6호 -> 2층: [1,1,1,1,1,1] -> 1층: [6,5,4,3,2,1] -> 0층: [21,15,10,6,3,1] (descending sum)
+<Variables>
+- T : the number of test case
+- k, n : floor, room number (1 <= k, n <= 14)
 
+<Algorithm>
+- Brute Force
 '''
 
 
 def people_num(house):
+    '''
+    ex) 3층의 6호 -> 2층: [1,1,1,1,1,1] -> 1층: [6,5,4,3,2,1] -> 0층: [21,15,10,6,3,1] (descending sum)
+    '''
     k, n = house[0]-1, house[1]
     cnt_list = [1]*n
     while k != 0:
