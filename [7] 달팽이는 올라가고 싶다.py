560966f92541d15e197b50cf980fbe2401469f85
lala991204 <oyunjeong7@gmail.com>
2024-06-08 15:06:48 +0900

    [백준] 2869. 달팽이는 올라가고 싶다

---
+++ [7] 달팽이는 올라가고 싶다.py
@@ -0,0 +1,30 @@
+'''
+<Description>
+There is the snail and the wooden stick.
+During the day, it could climb as much as A.
+At night, it might slip as much as B during sleeping.
+(If it arrive at the peak, it can't slip anymore.)
+Now, how much the days if it arrive at the peak?
+
+<Variables>
+- A : the length the snail could climb during the day
+- B : the length the snail might slip at night
+- V : the length of the wooden stick
+
+<Algorithm>
+- Brute Force
+
+'''
+
+def snail_count(A, B, V):
+    '''
+    (A-B)*n >= (V-A) => n >= (V-A)//(A-B)
+
+    '''
+    if (V-A)%(A-B) == 0:
+        return (V-A)//(A-B) + 1
+    else:   
+        return (V-A)//(A-B) + 2
+
+A, B, V = map(int, input().split())
+print(snail_count(A, B, V)) 
